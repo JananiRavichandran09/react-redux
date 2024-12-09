63b6a656d6d5c03fefd8c194bb4f9b52fcfe0923
Janani R <jr@csod.com>
2024-12-09 21:30:30 +0530

    created project setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
@@ -6,7 +5,7 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: './node_modules/.vite/react-redux',
+  cacheDir: './node_modules/.vite/shadcn-setup',
   server: {
     port: 4200,
     host: 'localhost',
@@ -16,12 +15,13 @@ export default defineConfig({
     host: 'localhost',
   },
   plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
+  resolve: {
+    alias: {
+      '@': '/src', // Manually define the alias
+    },
+  },
   build: {
-    outDir: './dist/react-redux',
+    outDir: './dist/shadcn-setup',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
@@ -35,7 +35,7 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: {
-      reportsDirectory: './coverage/react-redux',
+      reportsDirectory: './coverage/shadcn-setup',
       provider: 'v8',
     },
   },
